2e316f995e670ad8aefe558ca04959b0e32b325a
Nao ARGOUSE <nargouse@e1r4p14.42.fr>
2021-01-15 13:17:34 +0100

    correction atoi + ajout memcpy

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: nargouse <nargouse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/07 13:17:41 by nargouse          #+#    #+#             */
-/*   Updated: 2021/01/07 17:51:19 by nargouse         ###   ########.fr       */
+/*   Updated: 2021/01/15 12:04:00 by nargouse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,8 @@ int	ft_atoi(char *str)
 			|| str[i] == '\v' || str[i] == '\f')
 		i++;
 	s = 1;
-	while (str[i] < '0' || s > '9')
-	{
-		if (str[i] == '-')
-			s = -s;
-		if (str[i] != '-' && str[i] != '+')
-			return (0);
-		i++;
-	}
+	if (str[i] == '-')
+		s = -s;
 	while (str[i] <= '9' && str[i] >= '0')
 	{
 		n = n * 10 + (str[i] - '0');
--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,8 +6,27 @@
 /*   By: nargouse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/07 18:28:49 by nargouse          #+#    #+#             */
-/*   Updated: 2021/01/07 18:28:59 by nargouse         ###   ########.fr       */
+/*   Updated: 2021/01/15 13:15:55 by nargouse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t			i;
+	unsigned char	*dst2;
+	unsigned char	*src2;
+
+	i = 0;
+	dst2 = (unsigned char *)dst;
+	src2 = (unsigned char *)src;
+	if (n == 0 || dst == src)
+		return (dst);
+	while (i < n)
+	{
+		dst2[i] = src[i];
+		i++;
+	}
+	return (dst);
+}
